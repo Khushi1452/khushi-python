31306f6ac28d0726f45ea30a67449459fd395c0c
Khushi1452 <khushipal631@gmail.com>
2025-06-27 09:12:47 +0530

    4program

---
+++ 3program.py
@@ -0,0 +1,5 @@
+x=int(input("Enter age:"))
+if x>=18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
---
+++ 4program.py
@@ -0,0 +1,8 @@
+def func():
+    a=535334
+    b=674326784
+    
+    
+    print(a*b)
+
+func()
